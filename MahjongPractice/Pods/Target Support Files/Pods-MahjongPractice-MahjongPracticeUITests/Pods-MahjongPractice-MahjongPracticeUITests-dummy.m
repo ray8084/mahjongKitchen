487bad39ad3eb19df06f3db5eda2d265e213bf79
ray8084 <ray8084@users.noreply.github.com>
2021-10-01 12:57:00 -0700

    add revcat

---
+++ MahjongPractice/Pods/Target Support Files/Pods-MahjongPractice-MahjongPracticeUITests/Pods-MahjongPractice-MahjongPracticeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MahjongPractice_MahjongPracticeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MahjongPractice_MahjongPracticeUITests
+@end
