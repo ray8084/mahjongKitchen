3428923208ba8942143fdbcf2483d9a51dc898d8
ray8084 <ray8084@users.noreply.github.com>
2021-10-01 11:12:19 -0700

    new

---
+++ MahjongPractice/MahjongPractice/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  MahjongPractice
+//
+//  Created by Ray Meyer on 10/1/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ MahjongPractice/MahjongPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MahjongPractice
+//
+//  Created by Ray Meyer on 10/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
