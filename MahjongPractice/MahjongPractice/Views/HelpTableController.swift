93d2d402db11fe31280fb037854e16212f2254b6
ray <ray8084@gmail.com>
2023-04-19 09:36:55 -0700

    test

--- MahjongPractice/MahjongPractice/Views/HelpTableController.swift
+++ MahjongPractice/MahjongPractice/Views/HelpTableController.swift
@@ -28,6 +28,7 @@ class HelpTableController: NarrowViewController, UITableViewDelegate, UITableVie
         addDiscarding()
         addPassing()
         addEndCap()
+        // test
     }
     
     
