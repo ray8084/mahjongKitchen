a6d14c9b8e6676d803f9aaf342f89a6eefec02e4
ray8084 <ray8084@users.noreply.github.com>
2023-04-23 15:40:58 -0700

    hand controller

--- MahjongPractice/MahjongPractice/Views/HandsController.swift
+++ MahjongPractice/MahjongPractice/Views/HandsController.swift
@@ -12,6 +12,7 @@ class HandsController: NarrowViewController  {
 
     private var maj: Maj!
     private var handView = HandView()
+    private var filterSegmentControl: UISegmentedControl!
     
     init(maj: Maj, frame: CGRect, narrowViewDelegate: NarrowViewDelegate) {
         self.maj = maj
@@ -26,15 +27,16 @@ class HandsController: NarrowViewController  {
         maxWidth = 620
         narrowView()
         xOffset = (Int(view.frame.width) - maxWidth) / 2
+        
+        addFilterSegmentControl()
         addCloseButton()
         
-        handView.showCard(self, x: 30, y: 50, width: view.frame.width - 50, height: view.frame.height - 150, bgcolor: .black, maj: maj)
+        handView.showCard(self, x: 30, y: 60, width: view.frame.width - 50, height: view.frame.height - 150, bgcolor: .black, maj: maj)
         handView.isHidden = false
         view.addSubview(handView.cardView)
         handView.update(maj)
     }
     
-     
     
     // -----------------------------------------------------------------------------------------
     //
@@ -49,7 +51,22 @@ class HandsController: NarrowViewController  {
         closeButton.setImage(image, for: .normal)
         closeButton.imageView?.contentMode = .scaleAspectFit
         closeButton.addTarget(self, action: #selector(closeButtonAction), for: .touchUpInside)
-        self.view.addSubview(closeButton)
+        view.addSubview(closeButton)
+    }
+
+    func addFilterSegmentControl() {
+        let items = ["2023", "2468", "Like", "Add", "Quints", "Runs", "13579", "W&D", "369", "S&P"]
+        filterSegmentControl = UISegmentedControl(items: items)
+        filterSegmentControl.selectedSegmentIndex = 0
+        filterSegmentControl.frame = CGRect(x: 25, y: 20, width: maxWidth - 50, height: Int(filterSegmentControl.frame.height))
+        filterSegmentControl.addTarget(self, action: #selector(changeFilter), for: .valueChanged)
+        view.addSubview(filterSegmentControl)
+    }
+    
+    @objc private func changeFilter(sender: UISegmentedControl) {
+        switch( sender.selectedSegmentIndex ) {
+            default: print("here")
+        }
     }
     
 
